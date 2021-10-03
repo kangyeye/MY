484d687b68c5abe92a1d0b7a281c3a42bfe86cad
kangyeye <87250799+kangyeye@users.noreply.github.com>
2021-10-03 20:48:32 +0800

    Update main.c

--- week1/ch01/main.c
+++ week1/ch01/main.c
@@ -1,1 +1,31 @@
-asd
+#include<stdio.h>
+#include<stdlib.h>
+
+struct list {
+   int value;
+   struct list * next;
+};
+
+typedef struct list item;
+
+int main() {
+   item * current, * head;
+   int i;
+
+   head = NULL;
+
+   for(i=1;i<=10;i++) {
+      current = (item *)malloc(sizeof(item));
+      current->value = i;
+      current->next  = head;
+      head = current;
+   }
+   current = head;
+
+   while(current)
+   {
+      printf("%d\t", current->value);
+      current = current->next ;
+   }
+   getchar();
+}
