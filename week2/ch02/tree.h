8c6b1b992d640ab269ce2d9703fb57f489466835
kangyeye <87250799+kangyeye@users.noreply.github.com>
2021-10-10 21:34:19 +0800

    Update tree.h

--- week2/ch02/tree.h
+++ week2/ch02/tree.h
@@ -11,5 +11,8 @@ Position FindMax( SearchTree T );
 SearchTree Insert( ElementType X，SearchTree T );
 SearchTree Delete( ElementType X，SearchTree T );
 int Retrieve( Position P );
+void InorderTraverse(SearchTree T);//中序遍历
+void PreorderTraverse(SearchTree T);//先序遍历
+void postOrderTraverse(SearchTree T);//后序遍历
 
 #endif /*_Tree_H */
