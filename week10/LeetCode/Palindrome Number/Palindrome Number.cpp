f62941a1698596b5a5fedc33212f3c3ef5cfaf72
kangyeye <87250799+kangyeye@users.noreply.github.com>
2021-12-16 22:59:33 +0800

    Create Palindrome Number.cpp

---
+++ week10/LeetCode/Palindrome Number/Palindrome Number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s = to_string(x);    //将int转为string
+        for (int i = 0; i < s.length()/2; i++) {
+            if (s[i] != s[s.length() - 1 - i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
