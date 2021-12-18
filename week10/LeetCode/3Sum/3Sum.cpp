8bddac9db2f1ac7b9be95a519354bae84fb45289
kangyeye <87250799+kangyeye@users.noreply.github.com>
2021-12-18 14:54:30 +0800

    Update 3Sum.cpp

--- week10/LeetCode/3Sum/3Sum.cpp
+++ week10/LeetCode/3Sum/3Sum.cpp
@@ -8,10 +8,10 @@ public:
         }
         int n=nums.size();
         for(int i=0;i<n;i++){
-            if(nums[i]>0){
+            if(nums[i]>0){      //nums[i]>0 则sum>0;
                 break;
             }
-            if(i>0 && nums[i]==nums[i-1]){
+            if(i>0 && nums[i]==nums[i-1]){      //题目中要求去重，所以要判断这次选中的第一个数字是否和上一次选中的第一个数是否相同
                 continue;
             }
             int left=i+1;
@@ -19,10 +19,10 @@ public:
             while(left<right){
                 if(nums[i]+nums[left]+nums[right]==0){
                     Ans.push_back({nums[i],nums[left],nums[right]});
-                    while(left<right && nums[left]==nums[left+1]){
+                    while(left<right && nums[left]==nums[left+1]){      //去重
                         left=left+1;
                     }
-                    while(left<right and nums[right]==nums[right-1]){
+                    while(left<right and nums[right]==nums[right-1]){       //去重
                         right=right-1;
                     }
                         
