79b3ede53786ba543b6bf5189d4a22f5acb13a7b
kangyeye <87250799+kangyeye@users.noreply.github.com>
2022-01-23 11:13:17 +0800

    Create 47. Permutations II.cpp

---
+++ week15/LeetCode/47. Permutations II/47. Permutations II.cpp
@@ -0,0 +1,38 @@
+class Solution {
+private:
+    void dfs(const vector<int>& nums,vector<int>& used,
+             vector<int>& path,vector<vector<int>>& res){
+        if(path.size() == nums.size()){
+            res.emplace_back(path);
+            return;
+        } 
+        for(int i = 0; i < nums.size(); i++){
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) {
+                continue;
+            }
+            if(used[i] == true){
+                continue;
+            }
+            else{
+                used[i] = true;
+                path.emplace_back(nums[i]);
+                dfs(nums,used,path,res);
+                used[i] = false;
+                path.pop_back();
+            }
+        }
+    }
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        int len = nums.size();
+        vector<vector<int>> res;
+        sort(nums.begin(),nums.end());
+        if(len == 0){
+            return res;
+        }
+        vector<int> path;
+        vector<int> used(len);
+        dfs(nums,used,path,res);
+        return res;
+    }
+};
